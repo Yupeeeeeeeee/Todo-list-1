84fda1ff210bcc81f0662a9c7df33344ab8d730a
Yupeeeeeeeee <bodo9545@gmail.com>
2026-08-06 18:51:00 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+let title =document.getElementById("title");
+let taskInp = document.getElementById("taskInp");
+let addBtn = document.getElementById("addBtn");
+let message = document.getElementById("message");
+let taskCount = document.getElementById("taskCount");
+let taskList = document.getElementById("taskList");
+
+let count= 0;
+
+addBtn.addEventListener("click",function(){
+    let taskText = taskInp.value;
+    if(taskText === ""){
+        message.textContent = "please enter new task"
+        return;
+    }
+  let li = document.createElement("li");
+  li.textContent = taskText;
+  
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+
+    li.appendChild(de)
+
+    taskList.appendChild(li);
+
+    message.textContent="Task added successfully";
+    count++;
+    taskCount.textContent= count;
+
+    taskInp.value = "";
+
+    deleteBtn.addEventListener("click",function(){
+        li.remove();
+        count--;
+        taskCount.textContent = count;
+        message.textContent = "Task deleted";
+    });
+});
