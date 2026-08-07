fa1088d8d7aea9e9757b8d726d6afd24e0cc9ec2
Yupeeeeeeeee <bodo9545@gmail.com>
2026-08-08 00:17:34 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,45 +1,47 @@
-let title =document.getElementById("title");
+let title = document.getElementById("title");
 let taskInp = document.getElementById("taskInp");
 let addBtn = document.getElementById("addBtn");
 let message = document.getElementById("message");
 let taskCount = document.getElementById("taskCount");
 let taskList = document.getElementById("taskList");
 
-let count= 0;
+let count = 0;
 
 addBtn.addEventListener("click",function(){
     let taskText = taskInp.value;
+
     if(taskText === ""){
-        message.textContent = "please enter new task";
+        message.textContent = "Please Enter A New Task";
         return;
     }
-    let li = document.createElement("li");
-    let taskSpan = document.createElement("span");
-    taskSpan.textContent = taskText;
-  
-    let noteInp = document.createElement("input");
-    noteInp.type = "text";
-    noteInp.placeholder = "Add Note";
 
-    let deleteBtn = document.createElement("button");
+    let li = document.createElement(li);
+
+    let taskSpan = document.createElement(span);
+    span.textContent = taskText;
+
+    let deleteBtn = document.createElement(button);
     deleteBtn.textContent = "Delete";
 
-    li.appendChild(taskSpan);
+    let noteInp = document.getElementById(input);
+    noteInp.placeholder = "Add Note";
+     
+    taskList.appendChild(li);
+    
+    li.appendChild(span);
     li.appendChild(noteInp);
     li.appendChild(deleteBtn);
 
-    taskList.appendChild(li);
-
-    message.textContent="Task added successfully";
     count++;
-    taskCount.textContent= count;
-
-    taskInp.value = "";
+    taskCount.textContent = count;
+    message.textContent = "Task Added Successfully";
 
     deleteBtn.addEventListener("click",function(){
         li.remove();
+        
         count--;
         taskCount.textContent = count;
-        message.textContent = "Task deleted";
+
+        message.textContent= "Task deleted";
     });
 });
